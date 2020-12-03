4d4e581c1c66e6a9054b3d046fbb1afca684d96f
Jonathan SAHM <contact@johnstyle.fr>
2020-12-03 08:51:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,14 +12,6 @@ if [ ! -f composer.lock ]; then
   rm -v composer.phar
 fi
 
-if [ ! -f .gitignore ]; then
-  echo "/vendor/" > .gitignore
-fi
-
-if [ ! -f .rewrite.conf ]; then
-    wget "https://raw.githubusercontent.com/phpguild/docker-prestashop-starter/master/.rewrite.conf.dist" -O .rewrite.conf
-fi
-
 if [ ! -f public/composer.lock ]; then
   wget "https://github.com/PrestaShop/PrestaShop/releases/download/${PRESTASHOP_VERSION}/prestashop_${PRESTASHOP_VERSION}.zip" -O "prestashop_${PRESTASHOP_VERSION}.zip"
   unzip "prestashop_${PRESTASHOP_VERSION}.zip" -d prestashop
@@ -28,6 +20,14 @@ if [ ! -f public/composer.lock ]; then
   rm -v "prestashop_${PRESTASHOP_VERSION}.zip"
 fi
 
+if [ ! -f .gitignore ]; then
+  echo "/vendor/" > .gitignore
+fi
+
+if [ ! -f .rewrite.conf ]; then
+    wget "https://raw.githubusercontent.com/phpguild/docker-prestashop-starter/master/.rewrite.conf.dist" -O .rewrite.conf
+fi
+
 sed -i "s/myapp/${PROJECT_NAME}/g" Makefile
-sudo make setup/server
+sudo make setup/server && service nginx restart
 make install
